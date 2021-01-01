11bc3519ea11414c81eb24a38b3dceee239dde31
kenshiro <ken8329baske@gmail.com>
2021-01-01 21:40:13 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked!")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
@@ -32,8 +22,9 @@ const User = (props) => {
 return <div>Hi, I am {props.name}!, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
